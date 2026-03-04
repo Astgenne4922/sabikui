1d11b6487e2c48b0a9f0cddd4656a663c22a448f
Astgenne <daniele.candido@tutamail.com>
2026-03-04 17:45:34 +0100

    chore(docs): added documentation for panic scenarios and remove unnecessary expect on string conversion

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -9,9 +9,12 @@ use crate::gui::constants;
 ///
 /// |Platform | Value                                                      |
 /// | ------- | ---------------------------------------------------------- |
-/// | Linux   | `$XDG_CONFIG_HOME` or `$HOME`/.config/hash_functions       |
-/// | macOS   | `$HOME`/Library/Application Support/sabikui/hash_functions |
-/// | Windows | `{FOLDERID_RoamingAppData}`\sabikui\hash_functions         |
+/// | Linux   | `$XDG_CONFIG_HOME` or `$HOME/.config/hash_functions`       |
+/// | macOS   | `$HOME/Library/Application Support/sabikui/hash_functions` |
+/// | Windows | `{FOLDERID_RoamingAppData}\sabikui\hash_functions`         |
+///
+/// # Panics
+/// A panic occurs if the config directory can't be accessed
 fn get_directory() -> PathBuf {
     dirs::config_dir()
         .expect("There should be a config directory")
@@ -20,8 +23,10 @@ fn get_directory() -> PathBuf {
 }
 
 #[must_use]
-#[expect(clippy::missing_panics_doc, reason = "Panic by design")]
 /// Returns a vector containing the names of the available executables under `hash_functions` (without extension if present)
+///
+/// # Panics
+/// A panic occurs if the config directory can't be accessed
 pub fn get_hash_functions() -> Vec<String> {
     let mut hashes: Vec<String> = std::fs::read_dir(get_directory())
         .expect("The config directory should be readable")
@@ -33,12 +38,17 @@ pub fn get_hash_functions() -> Vec<String> {
 }
 
 #[must_use]
-#[expect(clippy::missing_panics_doc, reason = "Panic by design")]
 /// Iterates over `functions` to spawn the corresponding executable with [`Command`] to calculate the hash of the list of `files`.
 ///
 /// The files are passed to the executable as a temporary file named `input.txt`
 /// The result is a vector of vectors of hashes of the files, the outer vector having the same order as the `functions` slice and the inner vectors
 /// having  the same order as the `files` slice
+///
+/// # Panics
+/// A panics occurs if:
+/// - the temporary directory can't be created
+/// - the input file can't be created or written
+/// - the command can't be started or returned an error
 pub fn calculate_hash(functions: &[String], files: &[PathBuf]) -> Vec<Vec<String>> {
     let temp_dir = tempfile::tempdir().expect("The temporary directory should have been created");
     let mut temp_file = std::fs::File::create(temp_dir.path().join("input.txt")).expect("The full path should exist");
@@ -59,9 +69,9 @@ pub fn calculate_hash(functions: &[String], files: &[PathBuf]) -> Vec<Vec<String
             assert!(
                 hash.status.success(),
                 "{function} command returned an error -> {}",
-                String::from_utf8(hash.stderr).expect("Output from the executables should always be valid")
+                String::from_utf8_lossy(&hash.stderr)
             );
-            let binding = String::from_utf8(hash.stdout).expect("Output from the executables should always be valid");
+            let binding = String::from_utf8_lossy(&hash.stdout);
             output.extend(binding.lines().map(String::from));
 
             output
